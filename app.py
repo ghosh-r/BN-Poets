f8329355ecd4c4b3bb329d27dc80830c76e2047f
Ritobrata Ghosh <58001482+ghosh-r@users.noreply.github.com>
2021-07-19 15:02:43 +0530

    adding run button and max seq len

--- app.py
+++ app.py
@@ -2,13 +2,26 @@ import streamlit as st
 
 from transformers import pipeline
 
+st.set_page_config(page_title="🖋BN-Poets")
+st.title("Bengali Poets")
+
+st.sidebar.subheader("Select sequence length of poem.")
+max_len = st.sidebar.slider(
+    "Maximum length",
+    value=150,
+    min_value=100,
+    max_value=250,
+    help="The maximum length of the sequence to be generated."
+)
+
 poet = pipeline('text-generation',
         model='ghosh-r/robi-kobi',
         tokenizer='ghosh-r/bangla-gpt2')
 
-poem = poet('দরজা ধরে দাঁড়াও তুমি', max_length=250)
+if st.button("Write a poem"):
+    poem = poet('দরজা ধরে দাঁড়াও তুমি', max_length=max_len)
 
-output = poem[0]['generated_text'].replace('\n', ' ').replace('\xa0', '')
+    output = poem[0]['generated_text'].replace('\n', ' ').replace('\xa0', '')
 
 
-st.write(output)
+    st.write(output)
